9471d0f309ccdf45211993b72c6a93dee7539ae5
Tecnologie <dosal86@libero.it>
2025-02-06 12:28:44 +0100

    Create Query esercitazione.sql

---
+++ Query esercitazione.sql
@@ -0,0 +1,31 @@
+query amidate 
+SELECT 
+    (SELECT SUM(prezzo) FROM spedizione) AS SommaSpedizione,
+    (SELECT SUM(prezzo) FROM ordini) AS SommaOrdini
+FROM dual
+HAVING (SELECT SUM(prezzo) FROM spedizione) > (SELECT SUM(prezzo) FROM ordini);
+
+SELECT cliente_id, COUNT(*) AS numero_ordini
+FROM ordini
+GROUP BY cliente_id
+HAVING COUNT(*) > 5;
+
+
+SELECT 
+    (SELECT SUM(prezzo) FROM spedizione) AS SommaSpedizione,
+    (SELECT SUM(prezzo) FROM ordini) AS SommaOrdini
+WHERE (SELECT SUSELECT 
+    (SELECT SUM(prezzo) FROM spedizione) AS SommaSpedizione,
+    (SELECT SUM(prezzo) FROM ordini) AS SommaOrdini
+FROM dual
+HAVING (SELECT SUM(prezzo) FROM spedizione) > (SELECT SUM(prezzo) FROM ordini);M(prezzo) FROM spedizione) > (SELECT SUM(prezzo) FROM ordini);
+
+-- Questa query confronta la somma dei prezzi nelle tabelle 'spedizione' e 'ordini'
+SELECT 
+    -- Somma i prezzi nella tabella 'spedizione'
+    (SELECT SUM(prezzo) FROM spedizione) AS SommaSpedizione,
+    
+    -- Somma i prezzi nella tabella 'ordini'
+    (SELECT SUM(prezzo) FROM ordini) AS SommaOrdini
+FROM dual  -- Usa 'dual' per restituire un singolo risultato aggregato
+HAVING (SELECT SUM(prezzo) FROM spedizione) > (SELECT SUM(prezzo) FROM ordini);  -- Confronta le somme
