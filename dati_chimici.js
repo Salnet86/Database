452b1da2b116f45bd62b0cc1ca5f278d1fc17fa3
Tecnologie <dosal86@libero.it>
2025-04-06 18:41:43 +0200

    Add files via upload

---
+++ dati_chimici.js
@@ -0,0 +1,104 @@
+var chimica_d1 = {
+   materiale: "Materiale 1",
+   liquidi: "Liquido 1",
+   test: "Test 1",
+   codice: "Codice 1",
+   campione: "Campione 1"
+};
+
+var chimica_d2 = {
+   materiale: "Materiale 2",
+   liquidi: "Liquido 2",
+   test: "Test 2",
+   codice: "Codice 2",
+   campione: "Campione 2"
+};
+
+var chimica_d3 = {
+   materiale: "Materiale 3",
+   liquidi: "Liquido 3",
+   test: "Test 3",
+   codice: "Codice 3",
+   campione: "Campione 3"
+};
+
+var chimica_d4 = {
+   materiale: "Materiale 4",
+   liquidi: "Liquido 4",
+   test: "Test 4",
+   codice: "Codice 4",
+   campione: "Campione 4"
+};
+
+var chimica_d5 = {
+   materiale: "Materiale 5",
+   liquidi: "Liquido 5",
+   test: "Test 5",
+   codice: "Codice 5",
+   campione: "Campione 5"
+};
+
+var chimica_d6 = {
+   materiale: "Materiale 6",
+   liquidi: "Liquido 6",
+   test: "Test 6",
+   codice: "Codice 6",
+   campione: "Campione 6"
+};
+
+var chimica_d7 = {
+   materiale: "Materiale 7",
+   liquidi: "Liquido 7",
+   test: "Test 7",
+   codice: "Codice 7",
+   campione: "Campione 7"
+};
+
+var chimica_d8 = {
+   materiale: "Materiale 8",
+   liquidi: "Liquido 8",
+   test: "Test 8",
+   codice: "Codice 8",
+   campione: "Campione 8"
+};
+
+var chimica_d9 = {
+   materiale: "Materiale 9",
+   liquidi: "Liquido 9",
+   test: "Test 9",
+   codice: "Codice 9",
+   campione: "Campione 9"
+};
+
+var chimica_d10 = {
+   materiale: "Materiale 10",
+   liquidi: "Liquido 10",
+   test: "Test 10",
+   codice: "Codice 10",
+   campione: "Campione 10"
+};
+
+var count = 0;
+var cod = [];
+
+cod[count] = chimica_d1;
+count++;
+cod[count] = chimica_d2;
+count++;
+cod[count] = chimica_d3;
+count++;
+cod[count] = chimica_d4;
+count++;
+cod[count] = chimica_d5;
+count++;
+cod[count] = chimica_d6;
+count++;
+cod[count] = chimica_d7;
+count++;
+cod[count] = chimica_d8;
+count++;
+cod[count] = chimica_d9;
+count++;
+cod[count] = chimica_d10;
+
+console.log(cod);
